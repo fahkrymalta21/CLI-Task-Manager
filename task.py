5b2a616652cc64f77baf2f9d44aaa1c20e4980c9
fakhry <fakhry@akhdani.co.id>
2025-12-05 16:56:40 +0700

    feat: dump data task ke json

--- task.py
+++ task.py
@@ -1,1 +1,14 @@
-print("Hello World")
+import json
+
+data = {
+    "id": 1,
+    "description": "Belajar Python Dasar",
+    "status": "todo",
+    "createdAt": "2023-10-27 10:00"
+}
+
+with open("task.json", "w") as task:
+    json.dump(data, task, indent=4)
+
+# json_string = json.dumps(data, indent=4)
+# print(json_string)
