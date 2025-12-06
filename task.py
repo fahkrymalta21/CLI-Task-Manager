5b4727bcce8e5cb62aae48394de90f0930af9353
fahkrymalta21 <muhammadfahkry@gmail.com>
2025-12-06 23:08:19 +0700

    add: condotional logic

--- task.py
+++ task.py
@@ -1,14 +1,24 @@
 import json
 
-data = {
-    "id": 1,
-    "description": "Belajar Python Dasar",
-    "status": "todo",
-    "createdAt": "2023-10-27 10:00"
-}
+# data = {
+#     "id": 1,
+#     "description": "Belajar Python Dasar",
+#     "status": "todo",
+#     "createdAt": "2023-10-27 10:00"
+# }
 
-with open("task.json", "w") as task:
-    json.dump(data, task, indent=4)
+# with open("task.json", "w") as task:
+#     json.dump(data, task, indent=4)
 
 # json_string = json.dumps(data, indent=4)
 # print(json_string)
+
+perintah = ""
+while (perintah != "Stop"):
+    perintah = input("Perintah: ")
+    if (perintah == "Add"):
+        print("Tambah To Do List")
+    elif (perintah == "Remove"):
+        print("Hapus To Do List")
+    elif (perintah == "Edit"):
+        print("Edit To Do List")
